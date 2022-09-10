9164320b2e6e8793a945354063c69ba5cce7e66c
Brainware <freelancer7844@gmail.com>
2022-09-10 15:03:30 +0530

    file modified
    Signed-off-by: Brainware <freelancer7844@gmail.com>

---
+++ matrix.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10][10];
+    int a,b,c,d;
+
+    printf("Enter the no of rows of the matrix: ");
+    scanf("%d",&a);
+
+    printf("Enter the no of columns of the matrix: ");
+    scanf("%d",&b);
+
+
+}
